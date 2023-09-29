44dbfac432d1a0eaf0ee9e48fbbd0f3721ba05b7
Артем <artemkulichenko@joytech.store>
2023-09-29 20:41:00 +0400

    Initial commit

---
+++ LeetCodeProblems/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LeetCodeProblems;
+
+var solution = new Solution();
+
+// Console.WriteLine(solution.DifferenceOfSum(new []{11, 22, 33, 44}));
+
+// var arr = solution.SortByBits(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 });
+// arr = solution.SortByBits(new[] { 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1 });
+
+// foreach (var num in arr)
+// {
+//     Console.WriteLine(num); 
+// }
+
+
+// Console.WriteLine(solution.LastStoneWeight(new[]{2, 2}));
+
+// Console.WriteLine(solution.ChampagneTower(100000009, 33, 17));
+
+var matrix = new int[][]
+{
+    new []{1, 2, 3, 4},
+    new []{5, 6, 7, 8},
+    new []{9, 10, 11, 12},
+    new []{13, 14, 15, 16}
+};
+
+solution.Rotate(matrix);
+
+matrix.ToList().ForEach(item => Console.WriteLine(string.Join(", ", item.ToList())));
---
+++ LeetCodeProblems/Solution.cs
@@ -0,0 +1,128 @@
+﻿namespace LeetCodeProblems;
+
+public class Solution
+{
+    //https://leetcode.com/problems/difference-between-element-sum-and-digit-sum-of-an-array/description/
+    public int DifferenceOfSum(int[] nums)
+    {
+        var numSum = 0;
+        var digitSum = 0;
+
+        foreach (var item in nums)
+        {
+            var num = item;
+            
+            numSum += num;
+
+            var mod = 0;
+            var div = 0;
+
+            while (num > 0)
+            {
+                digitSum += num % 10;
+                num /= 10;
+            }
+        }
+
+        return numSum - digitSum;
+    }
+    
+    //https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/description/
+    public int[] SortByBits(int[] arr)
+    {
+        return arr.OrderBy(item => CountBites(item)).ThenBy(item => item).ToArray();
+    }
+
+    public int CountBites(int num)
+    {
+        var count = 0;
+        
+        while (num > 0)
+        {
+            var bitDecimalValue = (int)Math.Pow(2, (int)Math.Floor(Math.Log2(num)));
+            num -= bitDecimalValue;
+            count++;
+        }
+
+        return count;
+    }
+    
+    //https://leetcode.com/problems/last-stone-weight/description/
+    public int LastStoneWeight(int[] stones)
+    {
+        var stonesList = new List<int>(stones);
+        
+        var length = stones.Length;
+        
+        while (!length.Equals(1))
+        {
+            var secondStone = stonesList.Max();
+            stonesList.Remove(secondStone);
+
+            var firstStone = stonesList.Max();
+            stonesList.Remove(firstStone);
+            
+            stonesList.Add(secondStone - firstStone);
+            
+            length--;
+        }
+        
+        return stonesList.First();
+    }
+    
+    //https://leetcode.com/problems/champagne-tower/description/
+    
+    public double ChampagneTower(int poured, int query_row, int query_glass)
+    {
+        var glassesArray = new double[query_row + 2, query_row + 2];
+
+        //первый стакан заполняется отдельно
+        glassesArray[0, 0] = poured;
+        
+        for (var row = 0; row < query_row + 1; row++)
+        {
+            for (var glassIndex = 0; glassIndex < query_row + 1; glassIndex++)
+            {
+                //если стакан переполняется
+                if (glassesArray[row, glassIndex] > 1)
+                {
+                    //стакан становится полным
+                    var champagneOut = glassesArray[row, glassIndex] - 1;
+                    glassesArray[row, glassIndex] = 1;
+                    
+                    //шампанское стекает поровну на следующие стаканы под текущим
+                    glassesArray[row + 1, glassIndex] += champagneOut / 2;
+                    glassesArray[row + 1, glassIndex + 1] += champagneOut/ 2;
+                }
+                
+            }
+        }
+
+        return glassesArray[query_row, query_glass];
+    }
+
+    //https://vk.com/away.php?utf=1&to=https%3A%2F%2Fleetcode.com%2Fproblems%2Frotate-image%2Fdescription%2F
+    public void Rotate(int[][] matrix)
+    {
+        var rows = matrix.Length;
+        var columns = matrix[0].Length;
+        
+        for (var row = 0; row < rows; row++)
+        {
+            for (var column = row; column < columns; column++)
+            {
+                (matrix[row][column], matrix[column][row]) = (matrix[column][row], matrix[row][column]);
+            }
+        }
+        
+        for (var column = 0; column < columns/2; column++)
+        {
+            for (var row = 0; row < rows; row++)
+            {
+                (matrix[row][columns - 1 - column], matrix[row][column]) = (matrix[row][column], matrix[row][columns - 1 - column]);
+            }
+        }
+    }
+    
+}
+
